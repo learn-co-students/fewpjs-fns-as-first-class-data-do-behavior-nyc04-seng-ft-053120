e5e9c225f7b9bec67b1463b6d597edfe2645c601
kceron <karemiceron@gmail.com>
2020-08-03 19:04:13 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let parsedTime = parseInt(timeString, 10); //set variable and parse it
+  if (parsedTime < 12) { //if variable is < 12, it will display good morning
+    return 'Good Morning'
+  }
+  else if (parsedTime > 17) {
+    return 'Good Evening'
+  }
+  else {
+    return 'Good Afternoon'
+  }
+}
 /* Write your implementation of displayMessage() */
+//function that takes a string argument and updates the text inside #greeting node with content of the argument
+function displayMessage(content) {
+  let greeting = document.querySelector("#greeting")//setting variable greeting to = #greeting, which we find with querySelector()
+  greeting.innerText = `${content}`// call greeting variable and use function innerText to add content
+}
