edd1efef51eda009f872ee639c82dec791db8347
tracycollins <tc@threeceemedia.com>
2020-08-03 16:18:32 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,22 @@
 
 document.querySelector("button").addEventListener("click", handleClick);
 
+function greet(timeString) {
+  const timeParts = timeString.split(":")
+  console.log(timeParts)
+  if (parseInt(timeParts[0]) < 12) {
+    return 'Good Morning'
+  }
+  if (parseInt(timeParts[0]) >= 12 && parseInt(timeParts[0]) < 17) {
+    return 'Good Afternoon'
+  }
+  return 'Good Evening'
+}
+
+function displayMessage(message) {
+  document.querySelector("#greeting").innerText = message
+}
+
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
